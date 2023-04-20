62f5bf713575e014413b1d8eb90ced21cf2af85e
Banuyaswanth <mallinenibanu@gmail.com>
2023-04-20 11:35:31 +0000

    added a new api to get a particular book from the database with the help of request.params

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const sqlite3 = require("sqlite3");
 let dbpath = path.join(__dirname, "goodreads.db");
 let db = null;
 
+//Initializing the server and connecting to the database
 const initialiseDbandServer = async () => {
   try {
     db = await open({
@@ -23,9 +24,20 @@ const initialiseDbandServer = async () => {
 };
 initialiseDbandServer();
 
+//get books api
 app.get("/books/", async (request, response) => {
   let getbooksquery = `select * from book
     order by book_id;`;
   let booksarray = await db.all(getbooksquery);
   response.send(booksarray);
+});
+
+//get a particular book api
+
+app.get("/books/:bookid/", async (request, response) => {
+  const { bookid } = request.params;
+  const getbookquery = `select * from book 
+                            where book_id = ${bookid};`;
+  let book = await db.get(getbookquery);
+  response.send(book);
 });
